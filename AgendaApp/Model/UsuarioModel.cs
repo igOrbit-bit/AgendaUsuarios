8b0bfd69063a722f59f0ea116b48d2c7f0d00fe3
igOrbit-bit <igorosantos9090@gmail.com>
2025-03-24 09:36:03 -0300

    Sua mensagem de commit explicando as alterações

--- AgendaApp/Model/UsuarioModel.cs
+++ AgendaApp/Model/UsuarioModel.cs
@@ -9,16 +9,16 @@ namespace AgendaApp.Model
     public class UsuarioModel
     {
         private static List<Usuario> usuarios = new List<Usuario>();
-        //1️⃣ Armazena os produtos em uma lista(products).
+    
         public void addUsuario(Usuario usuario)
         {
             usuarios.Add(usuario);
-            //2️⃣ Permite adicionar novos produtos com addProduct().
+           
         }
         public List<Usuario> getUsuarios()
         {
             return usuarios;
-            //3️⃣ Permite recuperar a lista de produtos com getProducts().
+            
         }
     }
 }
